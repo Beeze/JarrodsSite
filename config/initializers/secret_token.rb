5dde7591a93df262cd4459d2f19a18ca6ec62b2d
beezebrown <m.brown298@gmail.com>
2015-09-27 11:04:59 -0500

    messing around adding files and stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JarrodSite::Application.config.secret_token = 'a2ac3b90604cd185a8ec38f7d5301dcfd0e85307f157928fb7d64e3715ec0d96ed61ee688d9d7a891327aad87e11dff94c4824d6943b522fb1b47dc984aeca31'
